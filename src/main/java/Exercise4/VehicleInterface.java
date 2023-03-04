59e8d7cba12f4d3cb5a408cfd8d6f4f8bac079c9
Luis Mendoza <luis.mendoza.alonso@hotmail.com>
2023-03-04 12:58:55 -0500

    Se realiza funcionalidad del ejercicio 4

---
+++ src/main/java/Exercise4/VehicleInterface.java
@@ -0,0 +1,15 @@
+
+package Exercise4;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Luis mendoza
+ */
+public interface VehicleInterface {
+    
+    public void createVehicle( ArrayList<Vehicle> vehicles);
+     public void showVehicle( ArrayList<Vehicle> vehicles);
+
+}
